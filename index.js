2e543ea8899757ab0874560a1bc482ffa0d3c3cc
Mason Reichbauer <mreichba@mail.usf.edu>
2019-11-16 16:23:52 -0500

    fixed nav bar

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 function burgerClick() {
   let links = document.getElementById('links');
-  if (links.style.display === 'none'){
-    links.style.display = 'block';  
+  if (links.className === 'links'){
+    links.className += ' null';  
   } else {
-    links.style.display = 'none';
+    links.className = 'links';
   }
     
 }
 
-
-
-// function showList() {
-//     let burgerIcon = document.getElementById("burger")
-//     if (burgerIcon.style.display === "none") {
-//         links.style.display = "inline";
-//     }
-// }
-
-// document.addEventListener("DOMContentLoaded", showList)
 document.addEventListener('DOMContentLoaded', burgerClick);
